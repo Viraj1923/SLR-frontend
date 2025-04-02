0240822d274fafa84bde8d141c33ca22c4b95f95
Viraj Mulik <virajmulik2304@gmail.com>
2025-04-02 21:27:45 +0530

    update config vite

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -1,20 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 5173,
-    strictPort: true,
-    cors: {
-      origin: 'https://slr-frontend.onrender.com', // Allow requests from this domain
-      credentials: true,
-    },
-  },
-  preview: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 4173,
-    strictPort: true,
-  },
-});
+})
