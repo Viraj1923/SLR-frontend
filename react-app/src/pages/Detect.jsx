e30231ab6535a3393ea7250d78414eb7e4fb4cca
Viraj Mulik <144205690+Viraj1923@users.noreply.github.com>
2025-04-21 23:05:55 +0530

    Update Detect.jsx

--- react-app/src/pages/Detect.jsx
+++ react-app/src/pages/Detect.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./Detect.css";
 
-const API_BASE = "https://slr-backend.onrender.com"; // ✅ Your deployed FastAPI backend
+const API_BASE = " http://127.0.0.1:8000"; // ✅ Your deployed FastAPI backend
 
 const Detect = () => {
   const videoRef = useRef(null);
