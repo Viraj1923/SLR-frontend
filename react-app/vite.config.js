d5bc682b798e94e16383cfb5b070fc4a730baac7
Viraj Mulik <144205690+Viraj1923@users.noreply.github.com>
2025-08-04 12:42:59 +0530

    Update vite.config.js

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -1,7 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // empty string (or '/' also works, but '' is safest)
+  build: {
+    outDir: 'dist',
+  },
 })
