a72c942af6399630cf3c5652f15ac05922284c73
Viraj Mulik <144205690+Viraj1923@users.noreply.github.com>
2025-08-04 12:36:15 +0530

    Update vite.config.js

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '', // empty string (or '/' also works, but '' is safest)
+  base: '/', // empty string (or '/' also works, but '' is safest)
 })
