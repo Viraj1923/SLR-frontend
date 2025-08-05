14a7d50c5b7ec9558505fe4c885e0d26f26af006
Viraj Mulik <144205690+Viraj1923@users.noreply.github.com>
2025-08-05 21:02:29 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-import "./App.css"
+import './App.css'
 import App from './App.jsx'
 import { AuthProvider } from './Authcontext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </StrictMode>,
+    <BrowserRouter basename="/">
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </StrictMode>
 )
