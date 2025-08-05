4305c1f1f5bcd2cfad895c5a9d867d4a637b8d15
Viraj Mulik <144205690+Viraj1923@users.noreply.github.com>
2025-08-05 21:30:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,9 +16,7 @@ import NotFound from "./pages/NotFound/NotFound";
 import Profile from "./pages/Profile/Profile";
 
 function App() {
-  useEffect(() => {
-    console.log('App rendered');
-  }, []);
+  console.log('App component loaded');
   return (
     <div className="app-container">
       <Router>
